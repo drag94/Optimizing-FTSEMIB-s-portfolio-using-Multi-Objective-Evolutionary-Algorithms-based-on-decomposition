b04fe803e14a6417cc44c757ced3a5b6bbd42afe
drag94 <59622737+drag94@users.noreply.github.com>
2021-03-29 17:06:50 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -5,7 +5,7 @@ import glob
 
 listavuota = []
 stock_names = []
-path_stock_files = '/home/davide/PycharmProjects/pythonProject2/genetic_algo_progetto/po_with_moead-levy_/dati ftsemib listino'
+path_stock_files = '/home/davide/PycharmProjects/pythonProject2/genetic_algo_progetto/po_moead/dati ftsemib listino'
 
 for filename in os.listdir(path_stock_files):
     print(filename)
@@ -27,14 +27,14 @@ df_final['Data'] = dates
 df_final = df_final.set_index(df_final['Data'], drop=True)
 df_final = df_final.sort_index()
 del df_final['Data']
-df_final.to_csv('genetic_algo_progetto/po_with_moead-levy_/dat/df_final1.txt',header=False,index=False)
+df_final.to_csv('genetic_algo_progetto/po_moead/dat/df_final1.txt',header=False,index=False)
 
 no_assets = int(df_final.shape[1])
 returns_mean = pd.DataFrame.mean(df_final).values.reshape(no_assets, 1)  # mean of returns
 returns_std = pd.DataFrame.std(df_final).values.reshape(no_assets, 1)  # std. of returns
 cov_matrix =pd.DataFrame.cov(df_final)
 df_ftsemib1 = pd.DataFrame([returns_mean[:,0],returns_std[:,0]]).transpose()
-df_ftsemib1.to_csv('/home/davide/PycharmProjects/pythonProject2/genetic algo progetto/po_with_moead-levy_/dat/df_ftsemib1.txt',
+df_ftsemib1.to_csv('/home/davide/PycharmProjects/pythonProject2/genetic algo progetto/po_moead/dat/df_ftsemib1.txt',
                    index=False, header=False)
 
 
